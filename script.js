cf3c72be85e38d6905738bd4fb329764869be3bf
robodone-dev <71869290+robodone-dev@users.noreply.github.com>
2022-07-26 17:17:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
+//キャンバスに画像を描画する
+function loadImage(id)
+{
+	//画像を読み込んでImageオブジェクトを作成する
+	var image = new Image();
+	image.src = 'https://robodone-dev.github.io/hyou-show/format.png';
+	image.onload = (function () {
+		//画像ロードが完了してからキャンバスの準備をする
+		var canvas = document.getElementById(id);
+		var ctx = canvas.getContext('2d');
+		//キャンバスのサイズを画像サイズに合わせる
+		canvas.width = image.width;
+		canvas.height = image.height;
+		//キャンバスに画像を描画（開始位置0,0）
+		ctx.drawImage(image, 0, 0);
+	});
+}
+//キャンバスに文字を描く
+function drawText(canvas_id, text_id)
+{
+	var canvas = document.getElementById(canvas_id);
+	var ctx = canvas.getContext('2d');
+	var text = document.getElementById(text_id);
+	//文字のスタイルを指定
+	ctx.font = '32px serif';
+	ctx.fillStyle = '#404040';
+	//文字の配置を指定（左上基準にしたければtop/leftだが、文字の中心座標を指定するのでcenter
+	ctx.textBaseline = 'center';
+	ctx.textAlign = 'center';
+	//座標を指定して文字を描く（座標は画像の中心に）
+	var x = (canvas.width / 2);
+	var y = (canvas.height / 2.5);
+	ctx.fillText(text.value, x, y);
+}
 
+
+function downloadCanvas(){
+    var canvas = document.getElementById("preview");
+    var link = document.createElement("a");
+    link.href = canvas.toDataURL("image/png");
+    link.download = "test.png";
+    link.click();
+}
