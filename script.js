b953a82b0e234d7cb32f700ebb456da1290b419b
robodone-dev <71869290+robodone-dev@users.noreply.github.com>
2022-07-27 13:46:47 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
-window.onload = function() {
-  var popup = document.getElementById('js-popup');
-  
+// ボタン、モダル、モダルの閉じるボタン、オーバーレイを変数に格納
+const btn = document.querySelector('.btn');
+const modal = document.querySelector('.modal');
+const closeBtn = document.querySelector('.close');
+const overlay = document.querySelector('.overlay');
+
+
+window.addEventListener('load', (event) => {
+  console.log('ページが完全に読み込まれました');
   loadImage('preview');
+});
+
   
-  if(!popup) return;
-  popup.classList.add('is-show');
 
-  var blackBg = document.getElementById('js-black-bg');
-  var closeBtn = document.getElementById('js-close-btn');
+// ボタンをクリックしたら、モダルとオーバーレイに.activeを付ける
+btn.addEventListener('click', function(e){
+  // aタグのデフォルトの機能を停止する
+  e.preventDefault();
+  　// モーダルとオーバーレイにactiveクラスを付与する
+  modal.classList.add('active');
+  overlay.classList.add('active');
+});
 
-  closePopUp(blackBg);
-  closePopUp(closeBtn);
+// モダルの閉じるボタンをクリックしたら、モダルとオーバーレイのactiveクラスを外す
+closeBtn.addEventListener('click', function(){
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
+});
 
-  function closePopUp(elem) {
-    if(!elem) return;
-    elem.addEventListener('click', function() {
-      popup.classList.remove('is-show');
-    })
-  }
-}
+// オーバーレイをクリックしたら、モダルとオーバーレイのactiveクラスを外す
+overlay.addEventListener('click', function() {
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
+});
 
 
 //キャンバスに画像を描画する
